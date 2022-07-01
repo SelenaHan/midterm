14c6769f68365b1b476ca33f0fe16557ddeeb8d2
SelenaHan <shuo.han@edu.sait.ca>
2022-06-30 20:22:07 -0600

    modify the App.js file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './component/Home';
+import About from './component/About';
+import Contact from './component/Contact';
+import Students from './component/Students';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About Us</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact Us</Link>
+            </li>
+            <li>
+              <Link to="/students">Students list</Link>
+            </li>
+          </ul>
+          <Routes>
+            <Route exact path='/' element={< Home />}></Route>
+            <Route exact path='/about' element={<About />}></Route>
+            <Route exact path='/contact' element={<Contact />}></Route>
+            <Route exact path='/students' element={<Students />}></Route>
+          </Routes>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
